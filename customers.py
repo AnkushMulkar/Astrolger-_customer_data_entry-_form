c6aefa3159461d31763493ee0341c3818e15691c
Machine Learning Engineer <ankushmulkar.it@gmail.com>
2023-04-09 18:28:35 +0530

    Update customers.py

--- customers.py
+++ customers.py
@@ -28,7 +28,7 @@ def add_customer(name, gender, birthdate, birthplace, birth_time, address, city,
 
 
 # Define a function to delete an existing customer from the Excel file
-def delete_customer('name'):
+def delete_customer(name):
     # Read the Excel file into a DataFrame
     try:
         df = pd.read_excel(EXCEL_FILE)
@@ -68,6 +68,37 @@ password = st.text_input('Enter password', type='password')
 if check_password(password):
     # Prompt the user to select an action
     action = st.selectbox('Select an action', ['Add Customer', 'Delete Customer'])
+    # Check the password
+if check_password(password):
+    # Prompt the user to select an action
+    action = st.selectbox('Select an action', ['Add Customer', 'Delete Customer'])
+
+    # Prompt the user to enter customer information
+    if action == 'Add Customer':
+        name = st.text_input('Enter customer name')
+        gender = st.selectbox('Select customer gender', ['Male', 'Female', 'Other'])
+        birthdate = st.date_input('Enter customer birthdate')
+        birthplace = st.text_input('Enter customer birthplace')
+        birth_time = st.text_input('Enter customer birth time')
+        address = st.text_input('Enter customer address')
+        city = st.text_input('Enter customer city')
+        state = st.text_input('Enter customer state')
+        country = st.text_input('Enter customer country')
+        occupation = st.text_input('Enter customer occupation')
+        times_of_visit = st.text_input('Enter customer times of visit')
+        type_of_question = st.selectbox('Select customer type of question',['Job','marriage', 'business','family related', 'Ratn','Vastushastra','career','other'])
+        number_of_questions = st.number_input('Enter number of questions', value=1)
+    elif action == 'Delete Customer':
+        name = st.text_input('Enter the name of the customer you want to delete')
+      
+    # Add the new customer when the "Add Customer" button is clicked
+    if action == 'Add Customer' and st.button('Add Customer'):
+        message = add_customer(name, gender, birthdate, birthplace, birth_time, address, city, state, country, occupation, times_of_visit, type_of_question, number_of_questions)
+        st.write(message)
+    # Delete the customer when the "Delete Customer" button is clicked
+    elif action == 'Delete Customer' and st.button('Delete Customer'):
+        message = delete_customer(name)
+        st.write(message)
 
     # Prompt the user to enter customer information
     if action == 'Add Customer':
