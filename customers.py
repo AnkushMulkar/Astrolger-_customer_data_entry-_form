49cdad9b6c2911fa4f46599f748abf5013e5fac2
Machine Learning Engineer <ankushmulkar.it@gmail.com>
2023-04-09 18:07:08 +0530

    Update customers.py

--- customers.py
+++ customers.py
@@ -82,7 +82,7 @@ if check_password(password):
         country = st.text_input('Enter customer country')
         occupation = st.text_input('Enter customer occupation')
         times_of_visit = st.text_input('Enter customer times of visit')
-        type_of_question = st.selectbox('Select customer type of question',['Job','marriage', 'business','family related', 'Ratn','Vastushastra','career'])
+        type_of_question = st.selectbox('Select customer type of question',['Job','marriage', 'business','family related', 'Ratn','Vastushastra','career','other'])
         number_of_questions = st.number_input('Enter number of questions', value=1)
 
     # Add the new customer when the "Add Customer" button is clicked
