ec02142f1c7918a2facbc3ee2e105efbf3340510
Machine Learning Engineer <ankushmulkar.it@gmail.com>
2023-04-09 18:37:24 +0530

    Update customers.py

--- customers.py
+++ customers.py
@@ -95,22 +95,6 @@ if check_password(password):
         message = delete_customer(name)
         st.write(message)
 
-    # Prompt the user to enter customer information
-    if action == 'Add Customer':
-        name = st.text_input('Enter customer name')
-        gender = st.selectbox('Select customer gender', ['Male', 'Female', 'Other'])
-        birthdate = st.date_input('Enter customer birthdate')
-        birthplace = st.text_input('Enter customer birthplace')
-        birth_time = st.text_input('Enter customer birth time')
-        address = st.text_input('Enter customer address')
-        city = st.text_input('Enter customer city')
-        state = st.text_input('Enter customer state')
-        country = st.text_input('Enter customer country')
-        occupation = st.text_input('Enter customer occupation')
-        times_of_visit = st.text_input('Enter customer times of visit')
-        type_of_question = st.selectbox('Select customer type of question',['Job','marriage', 'business','family related', 'Ratn','Vastushastra','career','other'])
-        number_of_questions = st.number_input('Enter number of questions', value=1)
-
     # Add the new customer when the "Add Customer" button is clicked
     if st.button('Add Customer'):
         message = add_customer(name, gender, birthdate, birthplace, birth_time, address, city, state, country, occupation, times_of_visit, type_of_question, number_of_questions)
